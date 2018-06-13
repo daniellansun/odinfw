4ba72dd33619b0db183b11dc0c472d8704b937c4
Daniel Sun <realbluesun@hotmail.com>
2018-06-13 17:39:26 +0800

    Add a grid to user form

--- src/main/java/com/hkbea/app/domain/User.java
+++ src/main/java/com/hkbea/app/domain/User.java
@@ -8,6 +8,12 @@ public class User implements Serializable {
     private String name;
     private String password;
 
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
     public String getId() {
         return id;
     }
--- src/main/java/com/hkbea/app/ui/forms/UserForm.java
+++ src/main/java/com/hkbea/app/ui/forms/UserForm.java
@@ -1,11 +1,14 @@
 package com.hkbea.app.ui.forms;
 
+import com.hkbea.app.domain.User;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -16,8 +19,24 @@ import static com.hkbea.odinfw.ui.UiUtils.i18n;
 
 @SpringView(name="UserForm")
 public class UserForm extends VerticalLayout implements View {
+    private Grid<User> grid = new Grid<>();
+
     @PostConstruct
     private void init() {
+        Panel fieldInputsPanel = createFieldInputsPanel();
+        this.addComponent(fieldInputsPanel);
+
+        grid.setWidth("100%");
+        grid.setVisible(false);
+
+        grid.addColumn(User::getId).setCaption(i18n("text.id"));
+        grid.addColumn(User::getName).setCaption(i18n("text.name"));
+        grid.addColumn(User::getPassword).setCaption(i18n("text.password"));
+
+        this.addComponent(grid);
+    }
+
+    private Panel createFieldInputsPanel() {
         HorizontalLayout fieldsHL = new HorizontalLayout();
 
         TextField idTF = new TextField(i18n("text.id"));
@@ -36,9 +55,22 @@ public class UserForm extends VerticalLayout implements View {
         fieldsHL.addComponent(passwordTF);
 
         Button queryBtn = new Button(i18n("text.query"));
+        queryBtn.addClickListener((Button.ClickListener) event -> {
+            grid.setItems(
+                    new User("1", "Daniel", "****"),
+                    new User("2", "Paul", "****"),
+                    new User("3", "John", "****")
+            );
+
+            grid.setVisible(true);
+        });
         fieldsHL.addComponent(queryBtn);
         fieldsHL.setComponentAlignment(queryBtn, Alignment.BOTTOM_RIGHT);
 
-        this.addComponent(fieldsHL);
+        Panel fieldInputsPanel = new Panel(fieldsHL);
+
+        fieldsHL.setMargin(true);
+
+        return fieldInputsPanel;
     }
 }
