bfcb5e27f48fb04a973d6cb3a8a68544005d144e
Daniel Sun <realbluesun@hotmail.com>
2018-06-28 18:47:02 +0800

    Integrate the mysql limit plugin of mybatis generator

--- src/main/java/com/hkbea/app/repositories/UserDatMapper.java
+++ src/main/java/com/hkbea/app/repositories/UserDatMapper.java
@@ -2,9 +2,10 @@ package com.hkbea.app.repositories;
 
 import com.hkbea.app.domain.UserDat;
 import com.hkbea.app.domain.UserDatExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface UserDatMapper {
